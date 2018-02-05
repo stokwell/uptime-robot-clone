899f2a05100d06bf389fa9f1206ed0c850f4a100
Vitaly Lyapunov <stokwell1@gmail.com>
2018-02-05 13:06:52 +0100

    smpt config for development and alerts mailer

--- app/mailers/alerts_mailer.rb
+++ app/mailers/alerts_mailer.rb
@@ -1,6 +1,8 @@
 class AlertsMailer < ActionMailer::Base
+  default from: 'from@example.com'
 
-  def bad_response_email(user, site)
+  def bad_response_email(site)
+    user = User.find_by(id: site.user_id)
     mail(to: user.email, subject: "Bad response for site with url: #{site.url}") do |format|
       format.text
       format.html
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,11 +25,27 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-  
+
   config.active_job.queue_adapter = :sidekiq
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = true
+
+  # Change mail delvery to either :smtp, :sendmail, :file, :test
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    domain: "gmail.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
+  # Specify what domain to use for mailer URLs
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
 
   config.action_mailer.perform_caching = false
 
